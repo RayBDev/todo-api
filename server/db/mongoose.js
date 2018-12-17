90d51a0791f42d0f464a8dc11ad46b59a8919f40
RayB <raybernard1337@gmail.com>
2018-12-17 10:04:22 -0700

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb://localhost:27017/TodoApp",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp",
   { useNewUrlParser: true }
 );
 
